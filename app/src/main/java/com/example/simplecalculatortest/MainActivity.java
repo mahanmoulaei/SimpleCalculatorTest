d2277d0c7950d6ea031df72a2ba094317fb435c8
Mahan <mahanmoulaei@gmail.com>
2021-12-09 02:27:30 -0500

    Around 50% Of The Project Is Done

--- app/src/main/java/com/example/simplecalculatortest/MainActivity.java
+++ app/src/main/java/com/example/simplecalculatortest/MainActivity.java
@@ -1,14 +1,153 @@
 package com.example.simplecalculatortest;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextCalculation;
+    TextView textViewGeneratedOperation;
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonDecimal, buttonDash, buttonClear, buttonGenerate, buttonQuit, buttonValidate, buttonShowAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); //Hides The Notification Bar
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide(); //Hides The Application Title
+
         setContentView(R.layout.activity_main);
+
+        InitializeScreenComponents();
+    }
+
+    private void InitializeScreenComponents() {
+        editTextCalculation = findViewById(R.id.editTextCalculation);
+        editTextCalculation.setText(null);
+
+        textViewGeneratedOperation = findViewById(R.id.textViewGeneratedOperation);
+        textViewGeneratedOperation.setText(null);
+
+        button0 = findViewById(R.id.button0);
+        button0.setOnClickListener(this);
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        button5 = findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+
+        button6 = findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+
+        button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(this);
+
+        button8 = findViewById(R.id.button8);
+        button8.setOnClickListener(this);
+
+        button9 = findViewById(R.id.button9);
+        button9.setOnClickListener(this);
+
+        buttonDecimal = findViewById(R.id.buttonDecimal);
+        buttonDecimal.setOnClickListener(this);
+
+        buttonDash = findViewById(R.id.buttonDash);
+        buttonDash.setOnClickListener(this);
+
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonClear.setOnClickListener(this);
+
+        buttonGenerate = findViewById(R.id.buttonGenerate);
+        buttonGenerate.setOnClickListener(this);
+
+        buttonQuit = findViewById(R.id.buttonQuit);
+        buttonQuit.setOnClickListener(this);
+
+        buttonValidate = findViewById(R.id.buttonValidate);
+        buttonValidate.setOnClickListener(this);
+
+        buttonShowAll = findViewById(R.id.buttonShowAll);
+        buttonShowAll.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.button0: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button0.getText().toString());
+                break;
+            }
+            case R.id.button1: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button1.getText().toString());
+                break;
+            }
+            case R.id.button2: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button2.getText().toString());
+                break;
+            }
+            case R.id.button3: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button3.getText().toString());
+                break;
+            }
+            case R.id.button4: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button4.getText().toString());
+                break;
+            }
+            case R.id.button5: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button5.getText().toString());
+                break;
+            }
+            case R.id.button6: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button6.getText().toString());
+                break;
+            }
+            case R.id.button7: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button7.getText().toString());
+                break;
+            }
+            case R.id.button8: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button8.getText().toString());
+                break;
+            }
+            case R.id.button9: {
+                editTextCalculation.setText(editTextCalculation.getText().toString() + button9.getText().toString());
+                break;
+            }
+            case R.id.buttonGenerate: {
+                Operation generatedOperation = MathOperationGenerator.GenerateOperation();
+                textViewGeneratedOperation.setText(String.valueOf(generatedOperation));
+                break;
+            }
+            case R.id.buttonClear: {
+                textViewGeneratedOperation.setText(null);
+                editTextCalculation.setText(null);
+                break;
+            }
+            case R.id.buttonQuit: {
+                System.exit(0);
+                break;
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/simplecalculatortest/MathOperationGenerator.java
@@ -0,0 +1,78 @@
+package com.example.simplecalculatortest;
+
+import java.util.Random;
+
+public class MathOperationGenerator {
+
+    //Trigger This Function To Generate A Random Operation
+    public static Operation GenerateOperation() {
+        Operation operation = new Operation();
+        return operation;
+    }
+}
+
+class Operation {
+
+    private int leftNumber;
+    private int rightNumber;
+    private Operator operator;
+    private final Random randomGenerator = new Random();
+    private static final int MIN_OPERATION_ELEMENT_VALUE = 1;
+    private static final int MAX_OPERATION_ELEMENT_VALUE = 100;
+
+    public Operation() {
+        this.leftNumber = getRandomIntegerInRange(MIN_OPERATION_ELEMENT_VALUE, MAX_OPERATION_ELEMENT_VALUE);
+        this.operator = Operator.values()[randomGenerator.nextInt(Operator.values().length)];
+        this.rightNumber = getRandomIntegerInRange(MIN_OPERATION_ELEMENT_VALUE, MAX_OPERATION_ELEMENT_VALUE);
+    }
+
+    public int getLeftNumber() {
+        return leftNumber;
+    }
+
+    public void setLeftNumber(int value) {
+        this.leftNumber = value;
+    }
+
+    public int getRightNumber() {
+        return rightNumber;
+    }
+
+    public void setRightNumber(int value) {
+        this.rightNumber = value;
+    }
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public String getOperatorDisplayValue() {
+        return operator.getDisplayValue();
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+
+    private int getRandomIntegerInRange(int min, int max) {
+        return randomGenerator.nextInt(max - min + 1) + min;
+    }
+
+    @Override
+    public String toString() {
+        return getLeftNumber() + (operator == null ? "" : " " + getOperatorDisplayValue()) + " " + getRightNumber();
+    }
+}
+
+enum Operator {
+    PLUS("+"), MINUS("-"), MULTIPLIER("*"), DIVIDER("/");
+    private String displayValue;
+
+    private Operator(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+}
